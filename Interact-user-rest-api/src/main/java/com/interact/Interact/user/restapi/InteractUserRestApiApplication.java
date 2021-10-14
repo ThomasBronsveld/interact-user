58555d014b2bb1f2bf41e5e6087401f5176af8ab
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2021-10-14 19:11:43 +0200

    Initial spring boot project

---
+++ Interact-user-rest-api/src/main/java/com/interact/Interact/user/restapi/InteractUserRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.interact.Interact.user.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractUserRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractUserRestApiApplication.class, args);
+	}
+
+}
